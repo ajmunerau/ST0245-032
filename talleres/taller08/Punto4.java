3b2f98d91d880aaa804660e54d985852814896a7
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2020-03-25 22:57:56 -0500

    Add files via upload

---
+++ talleres/taller08/Punto4.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Punto4 {
+    public static void cola(Queue<String> queue){
+        while(queue.peek()!=null){
+            System.out.println("Atendiendo a " + queue.poll());
+}
+}
+}
