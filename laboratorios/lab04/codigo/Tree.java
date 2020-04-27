988dbfd0741b8026dd5341dbce696a94247573f6
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2020-04-26 23:37:57 -0500

    Update Tree.java

--- laboratorios/lab04/codigo/Tree.java
+++ laboratorios/lab04/codigo/Tree.java
@@ -1,15 +1,15 @@
 public class Tree {
-    private static Node2 root = new Node2("Proyecto", null);
+    private static Nodo root = new Nodo("Proyecto", null);
     public static void insertion(String name, double size) {
-        Node2 n = new Node2(name, size, root);
+        Nodo n = new Nodo(name, size, root);
         root.addFile(n);
     }
     public static void insertion(String directory, String name, double size) {
         Node2 node = insertionAux(root, directory);
-        Node2 n = new Node2(name, size, node);
+        Nodo n = new Nodo(name, size, node);
         node.addFile(n);
     }
-    public static Node2 insertionAux(Node2 node, String name) {
+    public static Nodo insertionAux(Nodo node, String name) {
         if(node.getName().equals(name)) return node;
         for(int i = 0; i < node.getFilesSize(); i++)
             insertionAux(node.getFile(i), name);
@@ -23,7 +23,7 @@ public class Tree {
         String [] array = s.split("/");
         searchAux(array, root, 0);
     }
-    public static void searchAux(String [] array, Node2 node, int p) {
+    public static void searchAux(String [] array, Nodo node, int p) {
         if(node.getName().equals(array[p])) {
             for(int i = 0; i < node.getFilesSize();i++) {
                 System.out.println(node.getFile(i).getName());
