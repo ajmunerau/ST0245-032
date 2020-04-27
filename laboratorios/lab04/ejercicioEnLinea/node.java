324e887648409894096c846bc97320f6740e33ec
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2020-04-26 22:50:49 -0500

    Create node.java

---
+++ laboratorios/lab04/ejercicioEnLinea/node.java
@@ -0,0 +1,9 @@
+public class Nodo {
+    public nodo izq,der;
+    public int dato;
+    public Nodo(int dato) {
+        this.izq = null;
+        this.der = null;
+        this.dato = dato;
+    }
+}
